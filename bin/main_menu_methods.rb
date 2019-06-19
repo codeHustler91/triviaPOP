4f5ae1678558272674ce04d1011f15958ba63e0a
Fekade Senbeta <fekadesenbeta@gmail.com>
2019-06-19 15:26:31 -0600

    added the main menu method

--- bin/main_menu_methods.rb
+++ bin/main_menu_methods.rb
@@ -1,3 +1,12 @@
 def main_menu
-    puts "yay! main menu!"
+    puts "TriviaPOP Time! Lets Play!? y/n"
+        @user_response = gets.chomp
+    if @user_response == "y"
+        puts "Yay, lets get started!"
+        # display first question.....get_and_parse
+        #@parshed_hash
+    else @user_response == "n" 
+        puts `clear`
+        puts "Awww Okay :(  Maybe Next Time"
+    end
 end
